3e8c020329efb66678942bc95991cabb89eebb39
Alex <133882841+AlexanderS89@users.noreply.github.com>
2023-08-22 12:50:03 +0100

    Add game function to generate score from outcomes

--- javascript.js
+++ javascript.js
@@ -16,6 +16,7 @@ const gameChoices = ["Rock", "Paper", "Scissors"];
 let score = 0;
 let player
 
+//getComputerChoice generates a random number between 1 and 3.
 function getComputerChoice() {
     let computerSelection = Math.floor(Math.random() *3 +1);
     if (computerSelection === 1) {
@@ -28,6 +29,7 @@ function getComputerChoice() {
   return compSelect;
 }
 
+//playRound() plays a round of Rock,Paper,Scissors and determines the outcome.
 function playRound(compSelect, playerSelection){
     compSelect = getComputerChoice();
     let result = "";
@@ -35,14 +37,39 @@ function playRound(compSelect, playerSelection){
     if (compSelect === 'rock' && playerSelection === 'paper' ||
        compSelect === 'paper' && playerSelection === 'scissors' ||
        compSelect === 'scissors' && playerSelection === 'rock'){
-        result = `You Win! ${playerSelection} beats ${compSelect}`;
+        result = 'Win';
+        outputResult = `You Win! ${playerSelection} beats ${compSelect}`;
        } else if (compSelect === playerSelection){
-        result = `You Drew! ${playerSelection} draws with ${compSelect}`;
+        result = 'Draw';
+        outputResult = `You Drew! ${playerSelection} draws with ${compSelect}`;
        } else {
-        result = `You Lost! ${playerSelection} loses to ${compSelect}`;
+        result = 'Lost';
+        outputResult = `You Lost! ${playerSelection} gets beat by ${compSelect}`;
        }
-    console.log(result);
+    console.log(outputResult);
     return result;
 }
 
-playRound();
+//game() runs the game 5 times checking results and tallying up the result to output.
+function game(result, score){
+    let resultsList = [];
+    score = 0;
+    for(let i = 0; i < 5; i++){
+        result = playRound();
+        if(result === 'Win'){
+            resultsList.push('Win');
+            ++score;
+        } else if(result === 'Lost'){
+            resultsList.push('Lose');
+            --score;
+        } else {
+            resultsList.push('Draw');
+        }
+    }
+    console.log(resultsList);
+    console.log(`Your score is: ${score}`);
+  return score;
+}
+
+game();
+
